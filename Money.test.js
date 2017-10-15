555d146817993282221f8cc8a02c11f9fa925aac
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-10-15 16:32:31 +0900

    Add Dollar.equals()

--- Money.test.js
+++ Money.test.js
@@ -1,8 +1,12 @@
 import test from 'ava';
 import Dollar from './Dollar';
 
-test('test multiplication', (t) => {
+test('multiplication', async (t) => {
   const five = new Dollar(5);
   t.is(10, five.times(2).amount);
   t.is(15, five.times(3).amount);
+});
+
+test('equality', async (t) => {
+  t.true(new Dollar(5).equals(new Dollar(5)));
 });
