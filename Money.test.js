3c53e817b66e503575aef70d88038d95382c23d3
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-10-15 16:39:24 +0900

    Compare object directly

--- Money.test.js
+++ Money.test.js
@@ -3,8 +3,8 @@ import Dollar from './Dollar';
 
 test('multiplication', async (t) => {
   const five = new Dollar(5);
-  t.is(10, five.times(2).amount);
-  t.is(15, five.times(3).amount);
+  t.deepEqual(new Dollar(10), five.times(2));
+  t.deepEqual(new Dollar(15), five.times(3));
 });
 
 test('equality', async (t) => {
