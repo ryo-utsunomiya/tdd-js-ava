0bc2b359ef55586fd62a5fce046ffb0c78a95f22
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-10-15 16:58:02 +0900

    Refactor test

--- Money.test.js
+++ Money.test.js
@@ -3,8 +3,8 @@ import Dollar from './Dollar';
 
 test('multiplication', async (t) => {
   const five = new Dollar(5);
-  t.deepEqual(new Dollar(10), five.times(2));
-  t.deepEqual(new Dollar(15), five.times(3));
+  t.true(new Dollar(10).equals(five.times(2)));
+  t.true(new Dollar(15).equals(five.times(3)));
 });
 
 test('equality', async (t) => {
